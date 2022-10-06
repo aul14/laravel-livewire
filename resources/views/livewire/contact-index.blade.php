cf73e26e60c3ef073c62a2259bb33481008e0438
aul14 <rahmanaulia336@gmail.com>
2022-10-06 15:06:03 +0700

    delete with return confirm

--- resources/views/livewire/contact-index.blade.php
+++ resources/views/livewire/contact-index.blade.php
@@ -32,7 +32,7 @@
                     <td>
                         <button wire:click="getContact({{ $contact->id }})"
                             class="btn btn-sm btn-info btn-edit text-white">Edit</button>
-                        <button wire:click="destroy({{ $contact->id }})"
+                        <button wire:click="destroy({{ $contact->id }})" onclick="return confirm('Are you sure?')"
                             class="btn
                             btn-sm btn-danger text-white">Hapus</button>
                     </td>
